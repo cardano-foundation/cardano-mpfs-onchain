87313309ef582df1ad50a4ca7f9e0c8cc477235b
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-04-13 12:54:52 +0100

    chore: pin cardano-mpfs-cage to main after merge

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    cardano-mpfs-cage.url = "github:cardano-foundation/cardano-mpfs-cage/001-mixed-modify-reject";
+    cardano-mpfs-cage.url = "github:cardano-foundation/cardano-mpfs-cage";
   };
 
   outputs =
